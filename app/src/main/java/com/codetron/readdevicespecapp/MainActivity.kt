b5d645c528bae11215d720abc50c7e1d63400598
codetron <codetronclan@gmail.com>
2022-11-24 14:43:59 +0700

    update

--- app/src/main/java/com/codetron/readdevicespecapp/MainActivity.kt
+++ app/src/main/java/com/codetron/readdevicespecapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.codetron.readdevicespecapp
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.telephony.TelephonyManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
+
+//https://gist.github.com/AlaaEddinAlbarghoth/d2b61f3fef22dbd00507eba531a8a352
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,13 +25,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textDeviceID: TextView
     private lateinit var textSerial: TextView
 
-    private val requestId =
+    private val phoneStatePermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
             if (granted) {
-                Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show()
+                textDeviceID.text = getString(R.string.device_id, getDeviceId())
+                textSerial.text = getString(R.string.serial, getDeviceSerial(this))
             }
         }
 
+    private val phoneMultipleStatePermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it[Manifest.permission.READ_PHONE_STATE] == true && it[Manifest.permission.READ_PRECISE_PHONE_STATE] == true) {
+                textSerial.text = getString(R.string.serial, getDeviceSerial(this))
+            }
+        }
+
+    @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,18 +57,119 @@ class MainActivity : AppCompatActivity() {
         textBoard.text = getString(R.string.board, Build.BOARD)
         textRadio.text = getString(R.string.radio, Build.getRadioVersion())
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) ==
-            PackageManager.PERMISSION_GRANTED
+        textDeviceID.text = getString(R.string.device_id, getDeviceId())
+        textSerial.text = getString(R.string.serial, getDeviceSerial(this))
+    }
+
+    @SuppressLint("HardwareIds")
+    fun getDeviceId(): String? {
+        return if (ActivityCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.READ_PHONE_STATE
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
-            val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-            //textDeviceID.text = getString(R.string.device_id, tm.deviceId)
-            textSerial.text = getString(
-                R.string.serial,
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) Build.getSerial()
-                else Build.SERIAL
-            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Settings.Secure.getString(
+                    applicationContext.contentResolver,
+                    Settings.Secure.ANDROID_ID
+                )
+            } else {
+                val mTelephony = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+                mTelephony.deviceId
+            }
         } else {
-            requestId.launch(android.Manifest.permission.READ_PHONE_STATE)
+            phoneStatePermissionLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+            null
         }
     }
+
+    @SuppressLint("HardwareIds", "PrivateApi")
+    fun getDeviceSerial(applicationContext: Context): String? {
+        var serialNumber: String?
+
+        try {
+            val c = Class.forName("android.os.SystemProperties")
+            val get = c.getMethod("get", String::class.java)
+
+            serialNumber = get.invoke(c, "gsm.sn1") as String
+
+            when (serialNumber) {
+                "" -> serialNumber = get.invoke(c, "ril.serialnumber") as String
+            }
+
+            when (serialNumber) {
+                "" -> serialNumber = get.invoke(c, "ro.serialno") as String
+            }
+
+            when (serialNumber) {
+                "" -> serialNumber = get.invoke(c, "sys.serialnumber") as String
+            }
+
+            @Suppress("DEPRECATION")
+            when (serialNumber) {
+                "" -> serialNumber = Build.SERIAL
+            }
+
+            when (serialNumber) {
+                "" -> serialNumber = null
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(applicationContext, e.message, Toast.LENGTH_LONG).show()
+            serialNumber = null
+        }
+
+        if (serialNumber == "unknown") {
+            try {
+                val c = Class.forName("android.os.SystemProperties")
+                val get = c.getMethod(
+                    "get",
+                    String::class.java,
+                    String::class.java
+                )
+                serialNumber = get.invoke(c, "ril.serialnumber", "unknown") as String
+            } catch (ignored: Exception) {
+                Toast.makeText(applicationContext, "ignored ${ignored.message}", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && serialNumber == "unknown") {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.READ_PHONE_STATE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                serialNumber = Settings.Secure.getString(
+                    applicationContext.contentResolver,
+                    Settings.Secure.ANDROID_ID
+                )
+            } else {
+                phoneStatePermissionLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && serialNumber == "unknown") {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.READ_PHONE_STATE
+                ) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.READ_PRECISE_PHONE_STATE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                serialNumber = Build.getSerial()
+            } else {
+                phoneMultipleStatePermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.READ_PHONE_STATE
+                    )
+                )
+            }
+        }
+
+        return serialNumber
+    }
 }
+
